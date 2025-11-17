c53869cae1d959f2a18eb3826bdf6e1ae3009c19
Tsotne Tsirekidze <90200590+tsirekidze22@users.noreply.github.com>
2025-11-18 00:31:59 +0400

    variables

---
+++ index.js
@@ -0,0 +1,60 @@
+// Variables - Let, Const, Var
+// let firstName; // Declaration
+// firstName = "George"; // Initialization
+// firstName = "Tsotne"; // Assignment
+
+// const lastName = "Tsirekidze";
+
+// var age = 30;
+// age = 25;
+
+// // String
+// let companyName = "Geolab";
+// // console.log(companyName);
+
+// //  Number
+// let age = 29;
+// // console.log(age);
+
+// // Boolean
+// let isMonday = true;
+// let isFriday = false;
+
+// let userName;
+// console.log(userName);
+
+// let email = null;
+// console.log(email);
+// email = "abcd@gmail.com";
+
+// camelCase
+// snake_case
+
+const firstName = "George";
+const lastName = "Tsirekidze";
+
+const fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+let age = 25;
+
+// console.log(typeof fullName);
+// console.log(typeof age);
+// console.log(fullName - age);
+// NaN - Not a Number
+
+// alert("Welcome to website!");
+
+// const userResponse = confirm("Do you really want to leave the website?");
+
+// console.log(userResponse);
+// if(userResponse===true){
+
+// }
+
+// const usersAge = Number(prompt("What's your age?"));
+
+// console.log(usersAge);
+// console.log("Age in 2 years:", usersAge + 2);
+
+console.log(22 === "22");
